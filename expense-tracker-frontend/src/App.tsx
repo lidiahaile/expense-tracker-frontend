35ef5c86b61a7700125c25914c9030509a934acf
Lidia Haile <lidiahaile@gmail.com>
2024-08-01 04:55:11 +0100

    routes to login and register are added

--- expense-tracker-frontend/src/App.tsx
+++ expense-tracker-frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./Components/Login/Login"
+import Register from "./Components/Register/Register"
+
 
 const App = () => {
   return (
     <div>
-      <Login/>
-      
+        
+    <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                
+            </Routes>
+        </Router>
+              
     </div>
   )
 }
